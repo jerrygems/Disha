50a2827afedd7517624b322fc5cdf3d61c0aac38
jerrygems <shubham.debian@gmail.com>
2023-09-13 19:01:25 +0530

    update

--- app/index.js
+++ app/index.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Provider as PaperProvider } from 'react-native-paper';
-import Head from "../components/header/header"
-import IndexStyles from "./indexStyle"
-const Home = () => {
-    return (
-        <>
-            <PaperProvider>
-                <View style={IndexStyles.container}>
-                    <Head />
-                    <Text>Hello React Developer</Text>
-                </View>
-            </PaperProvider>
-        </>
-    )
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Header from '../routes/components/header/header'; // Adjust the import path accordingly
+
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  useEffect(() => {
+    loadFonts();
+  }, []);
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Header} />
+        {/* Define more screens and navigation here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default Home
+export default App;
