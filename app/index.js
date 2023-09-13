1dada7304ce8135ea710f2c519b7c92b5b97f1e4
jerrygems <shubham.debian@gmail.com>
2023-09-13 13:08:49 +0530

    update

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <>
+        <View>
+            <Text>Hello React Developer</Text>
+        </View>
+        </>
+    )
+}
+
+export default Home
---
+++ components/header/header.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import { View,Text } from "react-native-web";
+import styles from "./headerStyle";
+
+const header=()=>{
+    return (
+        <>
+        <View>
+            <Text>header is here Okay</Text>
+        </View>
+        </>
+    )
+}
