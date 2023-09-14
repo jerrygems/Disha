c15cbfc1585c65dfaf120c3cd987c77bed2d4d0e
jerrygems <shubham.debian@gmail.com>
2023-09-14 16:29:15 +0530

    update

--- public/components/Auth/AuthStuff.js
+++ public/components/Auth/AuthStuff.js
@@ -5,26 +5,28 @@ import { TextInput } from 'react-native-gesture-handler'
 
 const AuthStuff = () => {
     const [email, setEmail] = useState()
+    const [pass, setPass] = useState()
     return (
         <View style={styles.container}>
             <View style={styles.container1}>
                 <Image style={styles.image1} source={{ uri: "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2011&q=80" }} />
             </View>
             <View style={styles.container2}>
-                <View style={styles.cont2Child}>
+                <View style={styles.cont2Child1}>
                     <Text style={styles.heading}>LearnEra</Text>
                 </View>
                 <View style={styles.cont2Child2}>
-                    <TextInput style={styles.inp1} placeholder='Enter Email' value='email' onChangeText={text=> setEmail(text)}></TextInput>
-                    <TextInput style={styles.inp1} placeholder='Enter Password' value='password' onChangeText={text=> setPassword(text)}></TextInput>
+                    <TextInput style={styles.inp1} placeholder='Enter Email' value={email} onChangeText={text => setEmail(text)}></TextInput>
+                    <TextInput style={styles.inp1} placeholder='Enter Password' value={pass} onChangeText={text => setPass(text)}></TextInput>
                     <TouchableOpacity>
                         <LinearGradient style={styles.btn1} start={{ x: 0, y: 0.5 }} end={{ x: 1, y: 0.5 }} colors={['#e398ff', '#0077ff']}>
-                            <Text>Login/SignUp</Text>
+                            <Text style={{ fontSize: 18 }}>Login/SignUp</Text>
                         </LinearGradient>
                     </TouchableOpacity>
                 </View>
 
             </View>
+            
         </View>
     )
 }
@@ -47,6 +49,7 @@ const styles = StyleSheet.create({
         resizeMode: 'cover'
     },
     container2: {
+        position:'relative',
         flex: 0.7,
         marginTop: -20,
         paddingTop: 20,
@@ -57,32 +60,33 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     btn1: {
-        top: 20,
+        top: 30,
         paddingVertical: 8,
         paddingHorizontal: 20,
-        borderRadius: 10,
+        borderRadius: 35,
+        elevation: 5,
     },
-    cont2Child: {
+    cont2Child1: {
         margin: 0,
-        top: 0,
+        top: 60,
         padding: 0
     },
-    cont2Child2:{
-        top:40,
-        alignItems:'center',
+    cont2Child2: {
+        top: 180,
+        alignItems: 'center',
     },
     // extra stuff to use or make code more easy Xd
     heading: {
         fontSize: 30,
     },
-    inp1:{
-        backgroundColor:'white',
-        paddingHorizontal:15,
-        paddingVertical:8,
-        borderRadius:10,
-        width:'100%',
-        margin:5,
-
+    inp1: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        width: 300,
+        margin: 10,
+        elevation: 5
     }
 })
 export default AuthStuff
